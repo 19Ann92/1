04cb6340c9ebaebbab63035511f58ab91b536a37
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-24 22:11:00 +0300

    add Example 1

--- Example_1/Program.cs
+++ Example_1/Program.cs
@@ -9,7 +9,8 @@ int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
 
-
+Console.WriteLine(num1 > num2 ? $"Максимальное число {num1}." 
+: $"Максимальное число {num2}.");
 
 
 
