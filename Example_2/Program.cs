9123e04446512207751520f7dec4c9baf846d50e
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-24 22:28:43 +0300

    add Example 2 с рением задачи

--- Example_2/Program.cs
+++ Example_2/Program.cs
@@ -11,3 +11,15 @@ int num2 = int.Parse(Console.ReadLine());
 Console.Write("Введите первое число: ");
 int num3 = int.Parse(Console.ReadLine());
 
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int max = Max(num1, num2, num3);
+
+Console.WriteLine($"Максимальное число {max}.");
+
